94a101fdbea2bfa70295b38e71835901225289c3
craigh6247 <craig.hollington@gmail.com>
2021-07-06 10:11:23 +0100

    Update child_dir_anylser.ps1
    add registry key check

--- powershell/child_dir_anylser.ps1
+++ powershell/child_dir_anylser.ps1
@@ -1,6 +1,9 @@
- $FilterDate = (Get-Date).AddDays(-30)
+$FilterDate = (Get-Date).AddDays(-30)
  $targetDir = 'C:\Windows\System32\spool\'
+ $targetReg= 'Software\Microsoft\Windows\CurrentVersion\Run'
  $filehash = (Get-FileHash -Algorithm SHA1 -LiteralPath (Get-ChildItem -Recurse -File  -ErrorAction SilentlyContinue $targetDir|Where-Object {$_.LastWriteTime -ge $filterDate}|Sort-Object LastWriteTime).fullname |Format-Table -autosize -Property Hash, Path)
  $filePath = (Get-ChildItem -Recurse -File  -ErrorAction SilentlyContinue $targetDir |Where-Object {$_.LastWriteTime -ge $filterDate}|Sort-Object LastWriteTime|Where-Object {$_.LastWriteTime -ge $filterDate}|Sort-Object LastWriteTime |Format-Table -autosize -Property LastWriteTime, Name, Directory)
+ $registykey = (Get-ItemProperty -Path HKLM:$targetReg)
  $filehash |Out-File -FilePath C:\users\$env:UserName\fileoutput.txt 
- $filePath |Out-File -Append -FilePath C:\users\$env:UserName\fileoutput.txt 
+ $filePath |Out-File -Append -FilePath C:\users\$env:UserName\fileoutput.txt
+ $registykey |Out-File -Append -FilePath C:\users\$env:UserName\fileoutput.txt
